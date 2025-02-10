5a45f54e7446a800b9f3d7a362f2e938ef1846b1
Michael J. Kavulich, Jr. <kavulich@ucar.edu>
2025-02-10 21:33:38 +0000

    Update Jet module to updated spack-stack

--- modulefiles/met_vx/vx_jet.lua
+++ modulefiles/met_vx/vx_jet.lua
@@ -5,7 +5,7 @@ the NOAA RDHPC machine Jet
 
 whatis([===[Loads libraries needed for running HAFS METplus verification tasks on Jet ]===])
 
-prepend_path("MODULEPATH", "/mnt/lfs4/HFIP/hfv3gfs/role.epic/spack-stack/spack-stack-1.6.0/envs/unified-env-rocky8/install/modulefiles/")
+prepend_path("MODULEPATH", "/contrib/spack-stack/spack-stack-1.6.0/envs/fms-2024.01/install/modulefiles/")
 load ("Core/stack-intel/2021.5.0")
 load ("stack-intel-oneapi-mpi/2021.5.1")
 load ("metplus/5.1.0")
