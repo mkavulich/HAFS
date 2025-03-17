14944c340187657db3eef8c5463359c35e91d514
Michael J. Kavulich, Jr <kavulich@ucar.edu>
2025-03-17 11:56:24 -0500

    Updates for Orion: add modulefile, machine file, valid name
    Upgrade METplus to 6.0.0 for all platforms

--- modulefiles/met_vx/vx_jet.lua
+++ modulefiles/met_vx/vx_jet.lua
@@ -8,7 +8,7 @@ whatis([===[Loads libraries needed for running HAFS METplus verification tasks o
 prepend_path("MODULEPATH", "/contrib/spack-stack/spack-stack-1.6.0/envs/fms-2024.01/install/modulefiles/")
 load ("Core/stack-intel/2021.5.0")
 load ("stack-intel-oneapi-mpi/2021.5.1")
-load ("metplus/5.1.0")
+load ("metplus/6.0.0")
 
 load("rocoto")
 
--- modulefiles/met_vx/vx_orion.lua
+++ modulefiles/met_vx/vx_orion.lua
@@ -4,10 +4,11 @@ the NOAA RDHPC machine Hera
 ]])
 
 whatis([===[Loads libraries needed for running HAFS METplus verification tasks on Hera ]===])
-
-prepend_path("MODULEPATH", "/scratch1/NCEPDEV/nems/role.epic/spack-stack/spack-stack-1.6.0/envs/fms-2024.01/install/modulefiles/")
-load ("Core/stack-intel/2021.5.0")
-load ("stack-intel-oneapi-mpi/2021.5.1")
+load ("contrib")
+load ("noaatools")
+prepend_path("MODULEPATH", "/work/noaa/epic/role-epic/spack-stack/orion/spack-stack-1.6.0/envs/fms-2024.01/install/modulefiles/")
+load ("Core/stack-intel/2021.9.0")
+load ("stack-intel-oneapi-mpi/2021.9.0")
 load ("metplus/6.0.0")
 
 load("rocoto")
