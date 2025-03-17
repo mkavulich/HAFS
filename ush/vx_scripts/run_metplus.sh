3a7cdb720634de1f420f24e0675b576b0d12ebe1
Michael J. Kavulich, Jr <kavulich@ucar.edu>
2025-03-17 18:06:42 -0500

    Consolidate run scripts to a single bash script, add TCRMW logic

--- ush/vx_scripts/run_metplus.sh
+++ ush/vx_scripts/run_metplus.sh
@@ -1,40 +1,47 @@
 #!/bin/bash -l
-#set -x
+set -x
 
 #metplus_ROOT is set by loading metplus module, which should be done already
 export METPLUS_ROOT=$metplus_ROOT
+echo "Using METPLUS in $metplus_ROOT"
 
 # Import variables from var_defns.yaml
 . source_yaml.sh
-for sect in hafs platform user verification workflow; do
+sections=(
+  hafs
+  platform
+  user
+  verification
+  workflow
+)
+for sect in ${sections[*]} ; do
   source_yaml ${VAR_DEFNS_FP} ${sect}
 done
 
 # Set conf file names here
-CONF_FILE='tcpairs.conf'
+CONF_FILE="${METPLUSTOOLNAME}.conf"
 
 # Set the input and output directories here
 INPUT_DIR=${CDNOSCRUB}/${SUBEXPT}
 OUTPUT_DIR=${EXPTDIR}/${START_DATE}
 
-# Since TCPairs is set up to run once per cycle, the START_DATE will always equal the END_DATE
+# Since tasks are set up to run once per cycle, the START_DATE will always equal the END_DATE
 END_DATE=${START_DATE}
-INC=${OUTPUT_INC}
-#MODEL='OFCL'
+OUTPUT_INC_HR=$(($OUTPUT_INC / 3600))
+
+# Format for A-deck forecast track file
+ADECK_TEMPLATE="${STORM_ID}l.{init?fmt=%Y%m%d%H}.hfsa.trak.atcfunix"
 
 # We need to know if this is a HAFS-A or HAFS-B run; this comes from the HAFS "RUN" variable (how wonderfully descriptive)
 # The carets (^^) make the string all-caps
 MODEL=${RUN^^}
 
-# Format for A-deck forecast track file
-ADECK_TEMPLATE='{cyclone}l.{init?fmt=%Y%m%d%H}.hfsa.trak.atcfunix'
-
-# Export the variables
+# Export the variables needed for conf files
 export INPUT_DIR
 export OUTPUT_DIR
 export START_DATE
-export END_DATE
-export INC
+export FCST_LEN_HRS
+export OUTPUT_INC_HR
 export BASIN
 export STORM_ID
 export ADECK_TEMPLATE
@@ -42,9 +49,12 @@ export BEST_TRACK
 export LOG_MET_VERBOSITY
 export LOG_LEVEL
 export MODEL
+export FORECAST_DIR
+export VX_FCST_INPUT_BASEDIR
+export FCST_FN_TEMPLATE
 
-# Create experiment dir and run tcpairs
-mkdir -p ${OUTPUT_DIR}/tcpairs
-cd ${OUTPUT_DIR}/tcpairs
+# Create experiment dir and run specified metplus tool
+mkdir -p ${OUTPUT_DIR}/${METPLUSTOOLNAME}
+cd ${OUTPUT_DIR}/${METPLUSTOOLNAME}
 
 python ${METPLUS_ROOT}/ush/run_metplus.py -c ${SCRIPTSdir}/${CONF_FILE}
