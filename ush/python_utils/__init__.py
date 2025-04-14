ac6342e7f086c2cb169519d1d0a306591d991a29
Michael J. Kavulich, Jr <kavulich@ucar.edu>
2025-04-14 10:18:46 -0500

    Remove another unneeded function left over from SRW

--- ush/python_utils/__init__.py
+++ ush/python_utils/__init__.py
@@ -1,6 +1,4 @@
 from .check_for_preexist_dir_file import check_for_preexist_dir_file
-from .config_parser import (
-    check_structure_dict,
-)
+from .config_parser import check_structure_dict
 from .create_symlink import create_symlink
 from .environment import str_to_list
--- ush/python_utils/config_parser.py
+++ ush/python_utils/config_parser.py
@@ -3,26 +3,6 @@
 import os
 
 
-def structure_dict(dict_o, dict_t):
-    """Structure a dictionary based on a template dictionary
-
-    Args:
-        dict_o: dictionary to structure (flat one level structure)
-        dict_t: template dictionary used for structuring
-    Returns:
-        A dictionary with contents of dict_o following structure of dict_t
-    """
-    struct_dict = {}
-    for k, v in dict_t.items():
-        if isinstance(v, dict):
-            r = structure_dict(dict_o, v)
-            if r:
-                struct_dict[k] = r
-        elif k in dict_o.keys():
-            struct_dict[k] = dict_o[k]
-    return struct_dict
-
-
 def check_structure_dict(dict_o, dict_t):
     """Check if a dictionary's structure follows a template.
     The invalid entries are returned as a dictionary.
